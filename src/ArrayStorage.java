e55b1c3b43562908b921adf5eb233fa43a7bd9e7
neocod2020 <neocod2020@gmail.com>
2023-01-28 21:19:09 +0300

    Class ArrayStorage to fulfill HW1: created all given methods and private method arrange()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,67 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    public Resume[] storage = new Resume[10000];
 
-    void clear() {
+    private int size;
+
+
+    public void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
+        arrange();
+        storage[size] = r;
+        size++;
+        System.out.println("save resume: " + storage[size - 1] + ", size= " + size());
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].uuid)) {
+                storage[i] = null;
+                arrange();
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        arrange();
+        Resume[] valueArray = new Resume[size];
+        System.arraycopy(storage, 0, valueArray, 0, size);
+        return valueArray;
     }
 
-    int size() {
-        return 0;
+    private void arrange() {
+        Resume[] sortedArray = new Resume[storage.length];
+        size = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                sortedArray[size] = storage[i];
+                size++;
+            }
+        }
+        storage = sortedArray;
+    }
+
+    public int size() {
+        return size;
     }
 }
